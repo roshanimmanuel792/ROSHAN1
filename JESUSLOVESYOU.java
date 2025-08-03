ef482e59ac4c9a13a81fb5ceafffa05f1626e942
RohanBenielE <rohanbeniel110@gmail.com>
2025-08-03 15:48:04 +0530

    initial commit

---
+++ JESUSLOVESYOU.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JESUSLOVESYOU {
+    public static void main(String[] args) {
+        String JLY = "JESUS LOVES YOU";
+        Scanner a=new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = a.nextLine();
+        System.out.println("yes" + JLY + name);
+    }
+}
+    
+
